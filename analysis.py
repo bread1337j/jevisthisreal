7ac0f307b243da6dff788b24b3c4ec255e0e0a1b
bread1337 <git@bread1337.net>
2026-09-19 07:33:11 -0700

    bloatification

--- analysis.py
+++ analysis.py
@@ -1,10 +1,11 @@
 from typesafe_sdk import AsyncTypeSafeClient, Choice, Noul, Score
 import io
+import random
 async def analyzets(context: str) -> str:
     """analyzes ts"""
     async with AsyncTypeSafeClient() as client:
         response = await client.system_one(
-            state={"message": context},
+                state={"message": context },
             questions={
                 "realness": Noul(instructions="Is this real"),
                 "tone": Choice(
@@ -17,18 +18,40 @@ async def analyzets(context: str) -> str:
                 ),
                 "urgency": Score(
                     instructions="How urgent is this message?",
-                    criteria=["Can wait", "This week", "Today"],
+                    criteria=["Not at all", "Extremely"]
                 ),
+                "Coherence": Choice(
+                    instructions="How coherent is this message?",
+                    criteria={"Incomprehensible":None, "Hard to understand":None,
+                              "Somewhat understandable":None, "Coherent":None},
+               ),
+                "pdoom": Score(
+                    instructions="What is the probability of doom displayed by this message?",
+                    criteria=["0%", "100%"]
+                    ),
             },
         )
 
     builder = io.StringIO()
     builder.write("Detected realness: ")
-    builder.write(str(response.nouls["realness"].noul))
+    builder.write(f'{int((response.nouls["realness"].noul) * 100)}%')
     builder.write("\nTone: ")
     builder.write(str(response.choices["tone"].choice))
     builder.write("\nUrgency: ")
     builder.write(str(response.scores["urgency"].score))
+    builder.write("\nCoherency: ")
+    builder.write(str(response.choices["Coherence"].choice))
+    builder.write("\np(doom): ")
+    builder.write(str(response.scores["pdoom"].score))
+
+
+    builder.write("\nJev Quotient: ")
+    jevquotient = (response.nouls["realness"].noul * 100)
+    jevquotient /= (response.scores["urgency"].score * 5 + 1)
+    jevquotient *= response.scores["pdoom"].score
+    jevquotient += random.random() * 0.1
+    builder.write(f"{jevquotient:.5f}")
+
     result = builder.getvalue()
     builder.close()
     return result
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 from discord.ext import commands
 import os
+import io
 
 import importlib
 import analysis
